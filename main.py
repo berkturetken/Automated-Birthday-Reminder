fdfa2bf921b99ac4c3968bd447cb0c6dc21b4df8
Berk Türetken <berkturetken1997@hotmail.com>
2022-02-02 17:18:52 +0300

    Added Buket to the system

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ load_dotenv(local_key_path)
 MY_EMAIL = os.getenv("berkturetken1_gmail")
 PWD = os.getenv("berkturetken1_pwd")
 RECEIVER_EMAIL = "berkturetken@sabanciuniv.edu"
-RECEIVER_EMAIL_2 = "gokktugbasaran@gmail.com"
+RECEIVER_EMAIL_2 = "buket.karakas@aalto.fi"
+RECEIVER_EMAIL_3 = "gokktugbasaran@gmail.com"
 
 # Read the csv file
 data = pandas.read_csv("docs/birthdays.csv")
@@ -44,9 +45,15 @@ for person in people_birthday_dates:
                 to_addrs=RECEIVER_EMAIL,
                 msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
             )
-            # Send to Göktuğ
+            # Send to Buket
             connection.sendmail(
                 from_addr=MY_EMAIL,
                 to_addrs=RECEIVER_EMAIL_2,
+                msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
+            )
+            # Send to Göktuğ
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=RECEIVER_EMAIL_3,
                 msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
             )
