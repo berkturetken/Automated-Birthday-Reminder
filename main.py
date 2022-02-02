711ae2f29001882e1d352773f01875ce0c50ca76
Berk Türetken <berkturetken1997@hotmail.com>
2022-02-02 16:52:02 +0300

    Added Göktuğ to the system and changed the content of the email

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ load_dotenv(local_key_path)
 MY_EMAIL = os.getenv("berkturetken1_gmail")
 PWD = os.getenv("berkturetken1_pwd")
 RECEIVER_EMAIL = "berkturetken@sabanciuniv.edu"
+RECEIVER_EMAIL_2 = "gokktugbasaran@gmail.com"
 
 # Read the csv file
 data = pandas.read_csv("docs/birthdays.csv")
@@ -37,8 +38,15 @@ for person in people_birthday_dates:
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=MY_EMAIL, password=PWD)
+            # Send to me
             connection.sendmail(
                 from_addr=MY_EMAIL,
                 to_addrs=RECEIVER_EMAIL,
+                msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
+            )
+            # Send to Göktuğ
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=RECEIVER_EMAIL_2,
                 msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
             )
