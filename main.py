5492f7696e68c8e5c9d9bf8f09b1497d6570dd2c
Berk Türetken <berkturetken1997@hotmail.com>
2022-08-18 13:26:22 +0300

    Added surname of the person to the notification. Done some refactoring

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ load_dotenv(local_key_path)
 
 MY_EMAIL = os.getenv("berkturetken1_gmail")
 PWD = os.getenv("berkturetken1_pwd")
-RECEIVER_EMAIL = "berkturetken@sabanciuniv.edu"
-RECEIVER_EMAIL_2 = "buket.karakas@aalto.fi"
-RECEIVER_EMAIL_3 = "gokktugbasaran@gmail.com"
+receiver_emails = ["berk.turetken@aalto.fi", "buket.karakas@aalto.fi", "gokktugbasaran@gmail.com"]
 
 # Read the csv file
 data = pandas.read_csv("docs/birthdays.csv")
@@ -35,25 +33,15 @@ for person in people_birthday_dates:
             text = file.read()
             letter_final_version = text.replace("[NAME]", person["name"])
 
-        # Send the letter to the specified email
+        # Send the letter to the receivers
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=MY_EMAIL, password=PWD)
-            # Send to me
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=RECEIVER_EMAIL,
-                msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
-            )
-            # Send to Buket
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=RECEIVER_EMAIL_2,
-                msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
-            )
-            # Send to Göktuğ
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=RECEIVER_EMAIL_3,
-                msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
-            )
+
+            for receiver in receiver_emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=receiver,
+                    msg=f"Subject:Birthday Reminder!\n\n{letter_final_version}"
+                )
+
